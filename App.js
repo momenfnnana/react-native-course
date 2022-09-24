bd04360f23855622125dd25ee8ba173524a25ee0
Momen Fnnana <moamenfnnana1@gmail.com>
2022-09-24 12:06:44 +0300

    trying useMemo and useCallback

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import CustomComponent from "./CustomComponent";
+
+const ChildComponent = ({ title }) => (
+  <Text style={{ fontSize: 40 }}>{title}</Text>
+);
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [counter, setCounter] = useState("");
+  const person = useMemo(() => {
+    return {
+      name: value,
+    };
+  }, [value, counter]);
+  const personHandler = useCallback(() => {
+    setValue("momen");
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffect is working');
+  }, [value]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ChildComponent title="Text goes here" />
+      <CustomComponent />
+      <Button title="press here" onPress={personHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
