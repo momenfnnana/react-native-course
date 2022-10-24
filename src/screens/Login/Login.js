ee62b468cd173cc95e9ea789f7d3088fd54e88e5
Momen Fnnana <mommen@hemma.sa>
2022-10-24 09:03:40 +0300

    implementing post api call for login screen

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -10,17 +10,22 @@ import {
 import { Entypo } from "@expo/vector-icons";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { InputFiled } from "../../components";
 import styles from "./style";
+import { setAccessToken } from "../../axiosConfig";
 
 const loginSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Email is required"),
-  password: Yup.string()
-    .required("password is required")
-    .matches(
-      /^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!@#$%./^&*()_+<>,~`"':;]{8,}$/,
-      "Password must have at least 8 characters, one number, one special character"
-    ),
+  email: Yup.string()
+    // .email("Invalid email")
+    .required("Email is required"),
+  password: Yup.string().required("password is required"),
+  // .matches(
+  //   /^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!@#$%./^&*()_+<>,~`"':;]{8,}$/,
+  //   "Password must have at least 8 characters, one number, one special character"
+  // ),
 });
 
 const Login = () => {
@@ -49,17 +54,31 @@ const Login = () => {
   //   }
   // };
 
+  const onSubmitForm = (values, { ...rest }) => {
+    axios
+      .post("https://student.valuxapps.com/api/login", {
+        email: values.email,
+        password: values.password,
+      })
+      .then((response) => {
+        console.log(response);
+        setAccessToken(response.data?.data?.token);
+        AsyncStorage.setItem("accessToken", response.data?.data?.token);
+        setTimeout(() => {
+          rest.setSubmitting(false);
+        }, 3000);
+      })
+      .catch((error) => {
+        console.log({ error });
+      });
+    rest.resetForm({ email: "", password: "" });
+  };
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
         <Formik
-          initialValues={{ email: "", password: "" }}
-          onSubmit={(values, { ...rest }) => {
-            setTimeout(() => {
-              rest.setSubmitting(false);
-            }, 2000);
-            // rest.resetForm({ email: "mo@mom.asd", password: "asdasd" });
-          }}
+          initialValues={{ email: "abullah@gmailASD.com1", password: "123456" }}
+          onSubmit={onSubmitForm}
           validationSchema={loginSchema}
         >
           {({
@@ -70,64 +89,69 @@ const Login = () => {
             handleBlur,
             touched,
             isSubmitting,
-          }) => (
-            <>
-              <InputFiled
-                label="Email"
-                placeholder="janboon@gmail.com"
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-              />
-              {/* errors from Formik & yup */}
-              {errors.email && touched.email && (
-                <Text style={styles.errorMessage}>{errors.email}</Text>
-              )}
-              {/* regex error on email address */}
-              {/* {!isValidEmail(email) && (
+          }) => {
+            return (
+              <>
+                <InputFiled
+                  label="Email"
+                  placeholder="janboon@gmail.com"
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  value={values.email}
+                />
+                {/* errors from Formik & yup */}
+                {errors.email && touched.email && (
+                  <Text style={styles.errorMessage}>{errors.email}</Text>
+                )}
+                {/* regex error on email address */}
+                {/* {!isValidEmail(email) && (
                 <Text style={{ color: "red", fontSize: 12, margin: 15 }}>
                   not valid email
                 </Text>
               )} */}
-              <InputFiled
-                label="Password"
-                placeholder="******************"
-                secureTextEntry={isPasswordShown}
-                value={values.password}
-                onChangeText={handleChange("password")}
-                onBlur={handleBlur("password")}
-                rightIcon={
-                  isPasswordShown ? (
-                    <Entypo
-                      name="eye-with-line"
-                      size={24}
-                      color="#999999"
-                      onPress={showPasswordHandler}
-                    />
-                  ) : (
-                    <Entypo
-                      name="eye"
-                      size={24}
-                      color="#999999"
-                      style={styles.eye}
-                      onPress={showPasswordHandler}
-                    />
-                  )
-                }
-              />
-              {/* errors from Formik & yup */}
-              {errors.password && touched.password && (
-                <Text style={styles.errorMessage}>{errors.password}</Text>
-              )}
-              <Button
-                title="Submit"
-                onPress={handleSubmit}
-                disabled={isSubmitting}
-              />
-              {/* loader appears while submitting */}
-              {isSubmitting && <ActivityIndicator size={"large"} />}
-            </>
-          )}
+                <InputFiled
+                  label="Password"
+                  placeholder="******************"
+                  secureTextEntry={isPasswordShown}
+                  value={values.password}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  rightIcon={
+                    isPasswordShown ? (
+                      <Entypo
+                        name="eye-with-line"
+                        size={24}
+                        color="#999999"
+                        onPress={showPasswordHandler}
+                      />
+                    ) : (
+                      <Entypo
+                        name="eye"
+                        size={24}
+                        color="#999999"
+                        style={styles.eye}
+                        onPress={showPasswordHandler}
+                      />
+                    )
+                  }
+                />
+                {/* errors from Formik & yup */}
+                {errors.password && touched.password && (
+                  <Text style={styles.errorMessage}>{errors.password}</Text>
+                )}
+                {/* loader appears while submitting */}
+                {isSubmitting ? (
+                  <ActivityIndicator size={"large"} />
+                ) : (
+                  <Button
+                    title="Submit"
+                    onPress={handleSubmit}
+                    disabled={isSubmitting}
+                  />
+                )}
+              </>
+            );
+          }}
         </Formik>
       </View>
     </TouchableWithoutFeedback>
