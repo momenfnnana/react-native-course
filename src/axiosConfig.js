c6b8bc00326965520f041ffecf806b1bf90b8bcb
Momen Fnnana <mommen@hemma.sa>
2022-10-24 09:03:19 +0300

    add axios config file for configuring api calls

---
+++ src/axiosConfig.js
@@ -0,0 +1,26 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+export const setAccessToken = (token) => {
+  axios.defaults.headers.common = {
+    Authorization: token,
+  };
+};
+const readAccessToken = () => {
+  AsyncStorage.getItem("accessToken").then((res) => {
+    axios.defaults.headers.common = {
+      Authorization: res,
+    };
+  });
+};
+readAccessToken();
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error?.response?.status === 401) {
+    //   return RootNavigation.navigate("AuthFlow", { screen: "Login" });
+    }
+    // showErrorMessage(error?.response?.data?.Message);
+    throw error;
+  }
+);
